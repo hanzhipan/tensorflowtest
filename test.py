a59fcef6565d714a23d137c83803ec3e17da2add
hanzhipan <HUANGhongdan0823>
2017-11-13 14:20:15 +0800

    input_data

---
+++ test.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+#下载用于训练和测试的mnist数据集的源码
+
+import input_data # 调用input_data
+mnist = input_data.read_data_sets('data/', one_hot=True)
+print("type of minst %s" % (type(mnist)))
+print("number of train minst %d" % (mnist.train.num.examples))
+print("number of test minst %d" % (mnist.test.num.examples))
